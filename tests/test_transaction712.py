c92f25dd8264c0cfd35c4ba415466587bb2f7154
Viktor.Yastrebov <vyastrebov@vareger.com>
2022-06-20 12:37:23 +0300

    added testcase for Execute wrapped into Transaction712

--- tests/test_transaction712.py
+++ tests/test_transaction712.py
@@ -1,3 +1,4 @@
+import json
 from unittest import TestCase
 from eth_account import Account
 from eth_account.signers.local import LocalAccount
@@ -16,6 +17,7 @@ class TestTransaction712(TestCase):
     _TEST_ADDRESS = HexStr("0x7e5f4552091a69125d5dfcb7b8c2659029395bdf")
     _TEST_CHAIN_ID = 270
     _DEFAULT_NONCE = 0
+    _INCREMENT_FUNCTION_NAME = 'increment'
 
     def setUp(self) -> None:
         self.account: LocalAccount = Account.create(1)
@@ -37,6 +39,13 @@ class TestTransaction712(TestCase):
         with p.open(mode='r') as deploy_expected_file:
             lines = deploy_expected_file.readlines()
             self.deploy_contract_expected = "".join(lines)
+
+        p = Path('./counter_contract_abi.json')
+        with p.open(mode='r') as json_f:
+            self.counter_contract_abi = json.load(json_f)
+        self.w3 = Web3(Web3.EthereumTesterProvider())
+        self.counter_contract_instance = self.w3.eth.contract(abi=self.counter_contract_abi,
+                                                              bytecode=self.counter_contract)
 
     def test_withdraw_encode(self):
         withdraw = Withdraw(token_address=self.token.address,
@@ -62,6 +71,19 @@ class TestTransaction712(TestCase):
         ret = Transaction712.EIP_712_TX_TYPE + ret
         self.assertEqual(self.deploy_contract_expected, ret.hex())
 
-    # def test_encode_execute(self):
-    #     execute = Execute(contract_address=HexStr("0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"),
-    #                       call_data=)
+    def test_encode_execute(self):
+        encoded_function = self.counter_contract_instance.encodeABI(fn_name=self._INCREMENT_FUNCTION_NAME, args=[42])
+        contract_addr = HexStr("0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
+        if encoded_function.startswith("0x"):
+            encoded_function = encoded_function[2:]
+        init_addr = HexStr("0xe1fab3efd74a77c23b426c302d96372140ff7d0c")
+        execute = Execute(contract_address=contract_addr,
+                          call_data=bytes.fromhex(encoded_function),
+                          initiator_address=init_addr,
+                          fee=self.fee, nonce=0)
+        tx712 = Transaction712(execute, self._TEST_CHAIN_ID)
+        ret = tx712.as_rlp_values()
+        ret = Transaction712.EIP_712_TX_TYPE + ret
+        self.assertEqual("70f85a80808094eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee80a47cf5dab"
+                         "0000000000000000000000000000000000000000000000000000000000000002a"
+                         "82010e94eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee808080c0", ret.hex())
